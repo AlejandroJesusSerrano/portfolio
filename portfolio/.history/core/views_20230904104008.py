4470d527c7857356f9d76c309e9fbab0262e872f
AlejandroJesusSerrano <alejandrojserrano@hotmail.com>
2023-09-04 13:59:35 -0300

    begin templates

---
+++ portfolio/.history/core/views_20230904104008.py
@@ -0,0 +1,27 @@
+from django.shortcuts import render, HttpResponse
+
+html_header = """
+<h1>Mi Portfolio Personal</h1>
+  <ul>
+    <li> <a href="/">Inicio</a> </li>
+    <li> <a href="about-me/">Acerca de Mi</a> </li>
+    <li> <a href="portfolio/">Portfolio</a> </li>
+    <li> <a href="curriculum/">Curriculum</a> </li>
+    <li> <a href="contact/">Contáctame</a> </li>
+  </ul>
+"""
+# Create your views here.
+def home(request):
+  return HttpResponse(html_header + "<h3>Página Principal</h3>")
+
+def about(request):
+  return HttpResponse(html_header + "<h3>Acerca de Mi</h3><p>Mi nombre es Alejandro Jesús Serrano y soy programador")
+
+def portfolio(request):
+  return HttpResponse(html_header + "<h3>Portfolio</h3><p>Estos son los proyectos que he desarrollado</p>")
+
+def curriculum(request):
+  return HttpResponse(html_header + "<h3>Curriculum</h3><p>Especializaciones...</p><p>Futuros Conocimientos</p><p>Aprendiendo Actualmente</p>")
+
+def contact(request):
+  return HttpResponse(html_header + "<h3>Contacto</h3><p>Por favor complete el formulario de contacto</p>")
